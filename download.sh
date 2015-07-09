026d9c8b25b471491f05c3ff8f7f0660ef34bf9e
sinkcup <sinkcup@163.com>
2015-07-09 22:49:24 +0800

    refactory

--- download.sh
+++ download.sh
@@ -27,50 +27,41 @@ function download()
             i=0
             new_line=""
             http_code=200
+            md5_or_size=""
             for part in `echo $line | sed 's/|/ /g'`; do
-                echo $i
                 echo $part
                 #第一列必须是下载地址
                 if [ $i -eq 0 ]; then
                     uri=$part
                     origin_filename=`basename $uri`
+                    filename=$origin_filename
                 elif [ $i -eq 1 ]; then
                     #第2列必须是文件名，如果为空的话，将使用下载地址里相同的文件名
-                    #todo 如果为空的话，for循环结束了，不会进到这里……
-                    if [ $part = '' ]; then
-                        filename=$origin_filename
-                    else
-                        filename=$part
-                    fi
-                    http_code=`curl -sI "http://downloads.openwrt.io$relative_dir/$filename" | head -n 1 | awk '{print $2}'`
-                    # 把文件名都改成链接
-                    new_line=$filename
-                    if [ $http_code -ne 200 ]; then
-                        if [ ! -f $top_dir$relative_dir/$filename ]; then
-                            wget -O $top_dir$relative_dir/$filename $uri
-                        fi
-                    fi
+                    filename=$part
                 elif [ $i -eq 2 ]; then
                     #第3列可能是md5sum或者size
-                    if [ $http_code -ne 200 ]; then
-                        if [ $is_check_md5 -eq 1 ]; then
-                            expected_md5=$part
-                            md5=`md5sum $top_dir$relative_dir/$filename | awk '{print $1}'`
-                            if [ $expected_md5 != $md5 ]; then
-                                echo "error: md5 not match"
-                                exit 1
-                            fi
-                        fi
-                    fi
-                    new_line=$new_line'|'$part
-                    echo $new_line >> $top_dir$relative_dir/files.md
+                    #如果第2列为空，则第3列必须为空，否则第3变成了第2，会错乱
+                    md5_or_size=$part
                 fi
                 i=$(($i+1))
             done
+            http_code=`curl -sI "http://downloads.openwrt.io$relative_dir/$filename" | head -n 1 | awk '{print $2}'`
+            if [ $http_code -ne 200 ]; then
+                if [ ! -f $top_dir$relative_dir/$filename ]; then
+                    wget -O $top_dir$relative_dir/$filename $uri
+                fi
+                if [ $is_check_md5 -eq 1 ] && [ $md5_or_size != "" ]; then
+                    md5=`md5sum $top_dir$relative_dir/$filename | awk '{print $1}'`
+                    if [ $md5_or_size != $md5 ]; then
+                        echo "error: md5 not match"
+                        exit 1
+                    fi
+                fi
+            fi
+            echo $filename'|'$md5_or_size >> $top_dir$relative_dir/files.md
         done
     done
     return 0
-    exit
 }
 tmp=$(download $top_dir)
 echo "$tmp"
